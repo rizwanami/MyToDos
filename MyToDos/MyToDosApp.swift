920f7d2ac2ca13b1f6e824f81d625303f499aa21
Rizwana Ishaque <rizwana.ishaque@gmail.com>
2022-05-10 16:24:28 -0400

    Initial Commit

---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDosApp.swift
+//  MyToDos
+//
+//  Created by Rizwana on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
